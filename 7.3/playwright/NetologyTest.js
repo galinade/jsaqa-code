e9183aa7f7e0807f9e0bbfb93ae2b326785df1c7
Galina Demidova <galinagriz77@gmail.com>
2022-11-10 09:19:40 +0300

    commit jest

--- 7.3/playwright/NetologyTest.js
+++ 7.3/playwright/NetologyTest.js
@@ -4,7 +4,7 @@ const { chromium } = require("playwright");
   const browser = await chromium.launch({
     headless: false,
     slowMo: 5000,
-    devtools: true
+    devtools: true,
   });
   const page = await browser.newPage();
   await page.goto("https://netology.ru");
@@ -14,3 +14,16 @@ const { chromium } = require("playwright");
   //assertion
   await browser.close();
 })();
+const { test, expect } = require("@playwright/test");
+test("test", async ({ page }) => {
+  await page.goto("https://netology.ru/");
+  await page.getByRole("link", { name: "Войти" }).click();
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill("kleogr@bk.ru");
+  await page.getByPlaceholder("Пароль").click();
+  await page.getByPlaceholder("Пароль").fill("avvy4#77");
+  await page.getByTestId("login-submit-btn").click();
+  await expect(page).toHaveURL("https://netology.ru/profile");
+  await page.getByRole("heading", { name: "Мои курсы и профессии" }).click();
+});
