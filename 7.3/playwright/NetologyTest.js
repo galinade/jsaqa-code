6231336ef212d943f9f409bacf91cb5c0c739658
Galina Demidova <galinagriz77@gmail.com>
2022-11-15 22:17:59 +0300

    Проблемы с playweright

--- 7.3/playwright/NetologyTest.js
+++ 7.3/playwright/NetologyTest.js
@@ -14,16 +14,3 @@ const { chromium } = require("playwright");
   //assertion
   await browser.close();
 })();
-const { test, expect } = require("@playwright/test");
-test("test", async ({ page }) => {
-  await page.goto("https://netology.ru/");
-  await page.getByRole("link", { name: "Войти" }).click();
-  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
-  await page.getByPlaceholder("Email").click();
-  await page.getByPlaceholder("Email").fill("kleogr@bk.ru");
-  await page.getByPlaceholder("Пароль").click();
-  await page.getByPlaceholder("Пароль").fill("avvy4#77");
-  await page.getByTestId("login-submit-btn").click();
-  await expect(page).toHaveURL("https://netology.ru/profile");
-  await page.getByRole("heading", { name: "Мои курсы и профессии" }).click();
-});
--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -1,4 +1,46 @@
 const { test, expect } = require("@playwright/test");
+const { chromium } = require("playwright");
+const { email, password } = require("../user");
+
+test("test Successful authorization", async () => {
+  const browser = await chromium.launch({
+    headless: false,
+    slowMo: 5000,
+  });
+  const page = await browser.newPage();
+  await page.goto("https://netology.ru/");
+  await page.getByRole("link", { name: "Войти" }).click();
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill(email);
+  await page.getByPlaceholder("Пароль").click();
+  await page.getByPlaceholder("Пароль").fill(password);
+  await page.getByTestId("login-submit-btn").click();
+  await expect(page).toHaveURL("https://netology.ru/profile");
+  await page.getByRole("heading", { name: "Мои курсы и профессии" }).click();
+  await browser.close();
+})();
+
+test("test Unsuccessful authorization", async () => {
+  await page.goto("https://netology.ru/");
+
+  await page.getByRole("link", { name: "Войти" }).click();
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+
+  await page.getByPlaceholder("Email").click();
+
+  await page.getByPlaceholder("Email").fill("vhkkn@bk.ru");
+
+  await page.getByPlaceholder("Пароль").click();
+
+  await page.getByPlaceholder("Пароль").fill("bjhg44hh");
+
+  await page.getByTestId("login-submit-btn").click();
+
+  await page.getByTestId("login-error-hint").click();
+});
+
+//const { test, expect } = require("@playwright/test");
 
 test("test", async ({ page }) => {
   // Go to https://netology.ru/free/management#/
