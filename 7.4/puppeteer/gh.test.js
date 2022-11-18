102513454d571702cbd0dbc4db2199b01407cacd
Galina Demidova <galinagriz77@gmail.com>
2022-11-18 10:16:18 +0300

    puppeteer ???

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -5,30 +5,71 @@ beforeEach(async () => {
   await page.goto("https://github.com/team");
 });
 
-afterEach(() => {
-  page.close();
-});
-
 describe("Github page tests", () => {
-  test("The h1 header content'", async () => {
-    const firstLink = await page.$("header div div a");
-    await firstLink.click();
-    await page.waitForSelector('h1');
+  test("The h1 header content", async () => {
+    await page.setDefaultNavigationTimeout(60000);
+    const firstLink1 = await page.$("header div div a");
+    await firstLink1.click();
+    await page.waitForSelector("h1");
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
+    expect(title2).toEqual(
+      "GitHub for teams · Build like the best teams on the planet · GitHub"
+    );
   });
 
   test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    await page.setDefaultNavigationTimeout(600);
+    const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
   });
 
   test("The page contains Sign in button", async () => {
+    await page.setDefaultNavigationTimeout(60000);
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
       visible: true,
     });
-    const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
+    const actual = await page.$eval(btnSelector, (link) => link.textContent);
+    expect(actual).toContain("Get started with Team");
+  });
+});
+afterEach(() => {
+  page.close(600000);
+});
+
+beforeEach(async () => {
+  page = await browser.newPage();
+  await page.goto("https://github.com/pricing");
+});
+
+describe("Github page tests 2", () => {
+  test("The h1 header content 2", async () => {
+    await page.setDefaultNavigationTimeout(600);
+    const firstLink1 = await page.$(
+      "body > div.application-main > main > div.p-responsive.container-xl.text-center.mt-7.mt-md-8.mt-lg-9.mb-5.mb-lg-9 > h1"
+    );
+    await firstLink1.click();
+    await page.waitForSelector("h1");
+    const title3 = await page.title();
+    expect(title3).toEqual("Pricing · Plans for every developer · GitHub");
+  });
+
+  test("The first link attribute 2", async () => {
+    await page.setDefaultNavigationTimeout(600);
+    const actual = await page.$eval("a", (link) => link.getAttribute("href"));
+    expect(actual).toEqual("#start-of-content");
+  });
+
+  test("The page contains Sign in button 2", async () => {
+    await page.setDefaultNavigationTimeout(600);
+    const btnSelector = ".btn-muted-mktg.d-block.btn-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, (link) => link.textContent);
+    expect(actual).toContain("Join for free");
+  });
+  afterEach(() => {
+    page.close();
   });
 });
