e17dc18b7b44336060dc23efc9f6a78d0919c94b
Galina Demidova <galinagriz77@gmail.com>
2022-11-21 16:19:21 +0300

    playwright...

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require("@playwright/test");
 const { chromium } = require("playwright");
 const { email, password } = require("../user");
 
-test("test Successful authorization", async () => {
+test("Successful authorization", async () => {
   const browser = await chromium.launch({
     headless: false,
     slowMo: 5000,
@@ -22,6 +22,11 @@ test("test Successful authorization", async () => {
 })();
 
 test("test Unsuccessful authorization", async () => {
+  const browser = await chromium.launch({
+    headless: false,
+    slowMo: 5000,
+  });
+  const page = await browser.newPage();
   await page.goto("https://netology.ru/");
 
   await page.getByRole("link", { name: "Войти" }).click();
@@ -36,29 +41,8 @@ test("test Unsuccessful authorization", async () => {
   await page.getByPlaceholder("Пароль").fill("bjhg44hh");
 
   await page.getByTestId("login-submit-btn").click();
-
-  await page.getByTestId("login-error-hint").click();
-});
-
-//const { test, expect } = require("@playwright/test");
-
-test("test", async ({ page }) => {
-  // Go to https://netology.ru/free/management#/
-  await page.goto("https://netology.ru/free/management#/");
-
-  // Click a
-  await page.click("a");
-  await expect(page).toHaveURL("https://netology.ru/");
-
-  // Click text=Учиться бесплатно
-  await page.click("text=Учиться бесплатно");
-  await expect(page).toHaveURL("https://netology.ru/free");
-
-  page.click("text=Бизнес и управление");
-
-  // Click text=Как перенести своё дело в онлайн
-  await page.click("text=Как перенести своё дело в онлайн");
-  await expect(page).toHaveURL(
-    "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
+  await expect(page.locator("login-error-hint")).toContainText(
+    "Вы ввели неправильно логин или пароль"
   );
-});
+  await browser.close();
+})();
